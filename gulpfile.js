9433f876c78f4e5ff7634b9eb9c41f3002c67646
Ceasar15 <ceasarkwadwo@gmail.com>
2021-04-21 04:46:11 +0000

    http server

--- gulpfile.js
+++ gulpfile.js
@@ -23,16 +23,6 @@ const autoprefixer = require('gulp-autoprefixer');
 
 
 
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
-app.listen(port, host, function() {
-    console.log("Server started.......");
-  });
-
-
-
-
 
 
 /* Setup scss path */
